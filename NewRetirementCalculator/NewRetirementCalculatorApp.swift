f7108668a8104a5602872bcbabe4603826706065
MrSpockDe <MrSpockDe@googlemail.com>
2023-02-25 15:25:36 +0100

    Initial Commit

---
+++ NewRetirementCalculator/NewRetirementCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewRetirementCalculatorApp.swift
+//  NewRetirementCalculator
+//
+//  Created by Albert on 25.02.23.
+//
+
+import SwiftUI
+
+@main
+struct NewRetirementCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
