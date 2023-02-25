293746f2aeff042b42c613542f4c448b49486331
MrSpockDe <MrSpockDe@googlemail.com>
2023-02-25 15:42:10 +0100

    Added AppCenter framework

--- NewRetirementCalculator/NewRetirementCalculatorApp.swift
+++ NewRetirementCalculator/NewRetirementCalculatorApp.swift
@@ -6,9 +6,16 @@
 //
 
 import SwiftUI
+import AppCenter
+import AppCenterCrashes
+import AppCenterAnalytics
 
 @main
 struct NewRetirementCalculatorApp: App {
+    init() {
+        AppCenter.start(withAppSecret: "566711fe-c066-4dcf-8117-1b06e93e2c68", services: [Analytics.self, Crashes.self])
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
